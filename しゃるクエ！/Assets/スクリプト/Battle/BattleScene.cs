44ff833b19ba083d6f74ac71945cb753087dc0c4
akazaki <yuyuko9903@icloud.com>
2019-05-09 17:11:49 +0900

    キャラクターのステータス読み込み

--- しゃるクエ！/Assets/スクリプト/Battle/BattleScene.cs
+++ しゃるクエ！/Assets/スクリプト/Battle/BattleScene.cs
@@ -9,6 +9,13 @@ public class BattleScene : MonoBehaviour
     [SerializeField] private GameObject attackObj;
     [SerializeField] private GameObject receiveObj;
 
+    [SerializeField] private int pID1;
+    [SerializeField] private int pID2;
+    [SerializeField] private int pID3;
+    [SerializeField] private int eID1;
+    [SerializeField] private int eID2;
+    [SerializeField] private int eID3;
+
     public struct Action
     {
         //public Performance p;
@@ -48,7 +55,10 @@ public class BattleScene : MonoBehaviour
         {
             attackChara.GetComponent<Status>().SetSP(attackChara.GetComponent<Status>().GetSP() - mpCost);
             receiveChara.GetComponent<Status>().SetHP(receiveChara.GetComponent<Status>().GetHP() - atk);
-            Debug.Log(receiveChara.GetComponent<Status>().GetHP());
+
+            Debug.Log(receiveChara.gameObject.name + "に" + atk + "ダメージ！");
+            Debug.Log("残りHP" + receiveChara.GetComponent<Status>().GetHP());
+
             //if (abnormalState != AbnormalState.None)
             //{
             //List<AbnormalState> temp = receiveChara.GetComponent<Status>().abnormalSet;
@@ -58,4 +68,35 @@ public class BattleScene : MonoBehaviour
         }
     }
 
+    public int GetPID(int name)
+    {
+        if(name == 1)
+        {
+            return pID1;
+        }
+        else if (name == 2)
+        {
+            return pID2;
+        }
+        else
+        {
+            return pID3;
+        }
+    }
+
+    public int GetEID(int name)
+    {
+        if (name == 1)
+        {
+            return eID1;
+        }
+        else if (name == 2)
+        {
+            return eID2;
+        }
+        else
+        {
+            return eID3;
+        }
+    }
 }
