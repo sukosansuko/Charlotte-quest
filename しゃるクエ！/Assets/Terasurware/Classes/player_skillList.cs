94141d094b5207c4119805add5c88a64aa715db0
akazaki <yuyuko9903@icloud.com>
2019-05-10 20:04:25 +0900

    Add出来ない謎バグで時間... 覚悟の準備をしておいてくだい。

---
+++ しゃるクエ！/Assets/Terasurware/Classes/player_skillList.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class player_skillList : ScriptableObject
+{	
+	public List<Sheet> sheets = new List<Sheet> ();
+
+	[System.SerializableAttribute]
+	public class Sheet
+	{
+		public string name = string.Empty;
+		public List<Param> list = new List<Param>();
+	}
+
+	[System.SerializableAttribute]
+	public class Param
+	{
+		
+		public double ID;
+		public string charName;
+		public string skillName;
+		public double Lv;
+		public double power;
+		public string target;
+		public double sp;
+		public double period;
+		public string influence1;
+		public bool influence2;
+		public string Time;
+		public string effect;
+		public string etc;
+	}
+}
+
