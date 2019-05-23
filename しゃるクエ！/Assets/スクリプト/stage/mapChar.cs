44bd175ba538232b502d2a2d3fdadc6155cdc78e
a <a@a>
2019-05-23 09:47:10 +0900

    コミット

--- しゃるクエ！/Assets/スクリプト/stage/mapChar.cs
+++ しゃるクエ！/Assets/スクリプト/stage/mapChar.cs
@@ -9,12 +9,12 @@ public class mapChar : MonoBehaviour {
 
     public SpriteRenderer charImage;
     public static Vector3 InitCharPos;        // ｽﾃｰｼﾞﾏｯﾌﾟ上のｷｬﾗｸﾀｰ初期位置(talk1の場所)
-    public static Vector3 SavePos = new Vector3(0, 0, 10);
+    public static Vector3 SavePos = new Vector3(0, 0, 0);
     // Use this for initialization
     void Start()
     {
         charImage = GetComponent<SpriteRenderer>();
-        InitCharPos = new Vector3(GameObject.Find("talk1").transform.position.x, GameObject.Find("talk1").transform.position.y + 1.4f, 10);
+        InitCharPos = new Vector3(GameObject.Find("talk1").transform.position.x, GameObject.Find("talk1").transform.position.y + 1.4f, 0);
         charImage.transform.position = InitCharPos;
         activScene = SceneManager.GetActiveScene().name;
     }
@@ -22,7 +22,7 @@ public class mapChar : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (SavePos == new Vector3(0, 0, 10))
+        if (SavePos == new Vector3(0, 0, 0))
         {
             SavePos = InitCharPos;
         }
